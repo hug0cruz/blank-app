55c7d7a7c6f177457710cdf90f606f1398ad29dd
hug0cruz <97799825+hug0cruz@users.noreply.github.com>
2026-01-11 16:22:54 +0000

    Atualiza streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,7 +135,7 @@ st.markdown("""
   <svg class="icon-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7z"/>
   </svg>
-  <span class="title">FIELD MAP TOOLS V2.3 SKYLINE</span>
+  <span class="title">FIELD MAP TOOLS V2.4 SKYLINE+</span>
 </div>
 """, unsafe_allow_html=True)
 
